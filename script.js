d9cf626c4d1cf847ac09c7f10424512d1b56ba36
danizrafidz <daniz.rafidz@gmail.com>
2024-12-06 09:32:36 +0700

    update

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
+/**
+ * JS Basic:
+ * Primitive Data Type, Conditional (if & switch), Loop, Modular Functions, Array of Object
+ * 
+ * DOM:
+ * .getElementById(), .getElementsByClassName(), .getElementsByName()
+ * .createElement()
+ * .setAttribute()
+ * .remove()
+ * .append()
+ * .reset()
+ * .alert()
+ * .innerText
+ * window.location.href
+ * 
+ * localStorage (.getItem & .setItem)
+ * 
+ */
+
+// READ elements, 
 const textPertanyaan = document.getElementById("txt-pertanyaan")
 const form = document.getElementById("form-rad")
 const rad = document.getElementsByName("rad")
 const buttons = document.getElementsByClassName("buttons")[0]
-const submitOutput = document.getElementById("submit-output")
+const textInfo = document.getElementById("text-info")
+const prevBtn = document.getElementById("btn-prev")
 
 // READ hasil dari localStorage
 const namaUser = localStorage.getItem('namaUser');
-    
-// UPDATE hasil di <p> element dengan id 'nama-user'
-document.getElementById('nama-user').innerText = `Halo ${namaUser}!`|| 'Gagal';
 
+// UPDATE hasil di <p> element dengan id 'nama-user'
+document.getElementById('nama-user').innerText = `Halo ${namaUser}!` || 'Gagal';
+
+// DATABASE berupa Array of Object yang akan menyimpan semua data pertanyaan dan score
 let s = 0 // index untuk STORAGE
 let storage = [
     {
@@ -54,6 +76,34 @@ let storage = [
     },
 ]
 
+let feedbacks = {
+    'normal': [
+        'Selamat! Anda berada dalam kondisi mental yang normal. Tetap jaga kesehatan mental dengan rutin berolahraga, makan makanan bergizi, dan tidur cukup. Jangan lupa untuk meluangkan waktu untuk bersantai.',
+        'Anda berada dalam kondisi baik. Pastikan untuk tetap menjaga keseimbangan antara pekerjaan dan kehidupan pribadi agar kondisi ini dapat dipertahankan.',
+        'Kondisi mental Anda stabil. Terus lakukan aktivitas yang membuat Anda bahagia dan menjaga suasana hati tetap positif.'
+    ],
+    'stress ringan': [
+        'Anda mengalami stres ringan. Cobalah teknik relaksasi seperti meditasi atau pernapasan dalam untuk membantu mengurangi tekanan yang dirasakan.',
+        'Walaupun stres Anda masih tergolong ringan, penting untuk mengidentifikasi penyebabnya. Usahakan untuk berbicara dengan orang terdekat atau teman terpercaya untuk berbagi perasaan Anda.',
+        'Luangkan waktu untuk diri sendiri. Aktivitas seperti mendengarkan musik, membaca buku, atau berjalan santai dapat membantu mengurangi stres ringan ini.'
+    ],
+    'stress sedang': [
+        'Anda menunjukkan tanda-tanda stres sedang. Pertimbangkan untuk mengurangi beban kerja atau mengambil waktu untuk liburan singkat agar dapat merasa lebih rileks.',
+        'Stres Anda membutuhkan perhatian lebih. Cobalah untuk membuat jadwal harian yang lebih teratur dan prioritaskan kegiatan yang mendukung kesejahteraan mental Anda.',
+        'Diskusikan situasi Anda dengan teman, keluarga, atau konselor profesional jika memungkinkan. Hal ini dapat membantu meringankan beban pikiran yang sedang Anda rasakan.'
+    ],
+    'stress cukup berat': [
+        'Anda mengalami stres yang cukup berat. Sangat disarankan untuk mulai mempraktikkan gaya hidup sehat seperti tidur yang cukup, menghindari kafein berlebihan, dan berolahraga ringan.',
+        'Pertimbangkan untuk mencari bantuan profesional, seperti konselor atau psikolog, untuk mengatasi tekanan yang sedang Anda alami.',
+        'Luangkan waktu untuk benar-benar beristirahat dan menjauh dari penyebab stres. Fokus pada diri Anda sendiri untuk sementara waktu agar pikiran lebih jernih.'
+    ],
+    'stress berat': [
+        'Anda berada dalam kondisi stres berat. Jangan ragu untuk segera menghubungi tenaga profesional, seperti psikolog atau psikiater, untuk mendapatkan dukungan dan penanganan yang tepat.',
+        'Cobalah untuk berbicara dengan seseorang yang Anda percayai. Dukungan dari keluarga atau teman dekat sangat penting dalam situasi seperti ini.',
+        'Jangan terlalu keras pada diri sendiri. Beri tubuh dan pikiran Anda waktu untuk pulih. Jika memungkinkan, ambil cuti atau jeda dari rutinitas sehari-hari yang membebani Anda.'
+    ]
+}
+
 // READ check radio button mana yang dipilih dan mengembalikan berupa value
 function getCheckedValue() {
     for (let i = 0; i < rad.length; i++) {
@@ -64,7 +114,7 @@ function getCheckedValue() {
     return null
 }
 
-// SAVE ke dalam storage berupa array of object
+// SAVE value score ke dalam storage berupa number
 function saveToStorage() {
     let checkedValue = getCheckedValue();
     storage[s].score = checkedValue
@@ -73,42 +123,49 @@ function saveToStorage() {
 function submitButton() {
     saveToStorage()
 
+    // KALKULASI total score
     let scoreTotal = 0
     for (let i = 0; i < storage.length; i++) {
         let perNum = storage[i].score
-        
+
+        // JIKA ada opsi yang belum dipilih
         if (perNum === null) {
-            return submitOutput.innerText = 'Isi semua pertanyaan!'
+            return textInfo.innerText = 'Isi semua pertanyaan!'
         }
 
+        // JIKA pertanyaan 4, 5, 7, 8. MAKA tukar nilai score-nya
         if (i == 3 || i == 4 || i == 6 || i == 7) {
             perNum = 4 - perNum
         }
-        
         scoreTotal += perNum
     }
 
+    // menentukan textResult berdasarkan total score
     let textResult = ''
     if (scoreTotal >= 21) {
-        textResult = 'Anda mengalami stress berat'
+        textResult = 'stress berat'
     } else if (scoreTotal >= 16) {
-        textResult = 'Anda mengalami stress cukup berat'
+        textResult = 'stress cukup berat'
     } else if (scoreTotal >= 12) {
-        textResult = 'Anda mengalami stress sedang'
+        textResult = 'stress sedang'
     } else if (scoreTotal >= 8) {
-        textResult = 'Anda mengalami stress ringan'
+        textResult = 'stress ringan'
     } else if (scoreTotal >= 0) {
-        textResult = 'Anda normal'
+        textResult = 'normal'
     }
 
-    // // Simpan hasil di localStorage
-    localStorage.setItem('stressResult', textResult);
+    let r = Math.ceil(Math.random() * 3) - 1
+    stressResult = feedbacks[textResult][r]
 
-    // Redirect ke submit.html
+    // UPDATE hasil di localStorage
+    localStorage.setItem('stressResult', stressResult);
+    localStorage.setItem('scoreTotal', scoreTotal);
+
+    // redirect ke submit.html
     window.location.href = 'submit.html';
 }
 
-// READ radio mana yang ter-CHECKED pada pertanyaan sebelum, dilihat dari index storage []
+// READ dan UPDATE radio mana yang ter-CHECKED pada pertanyaan sebelum, dilihat dari index storage []
 function readCheckedRadio() {
     switch (storage[s].score) {
         case 0:
@@ -132,36 +189,43 @@ function readCheckedRadio() {
     }
 }
 
+/** BERLAKU UNTUK prevButton() & nextButton()
+ * SAVE [value] ke dalam [index] storage WITH function saveToStorage()
+ * UP / DOWN [index] storage
+ * READ [index] storage dan UPDATE checked radio WITH function readCheckedRadio()
+ */
+
+/** CONDITION:
+ * MIN index 0
+ * JIKA index 8, MAKA 
+ *      DELETE tombol submit
+ *      CREATE tombol next 
+ */
 function prevButton() {
     if (s <= 0) {
         return
     }
 
-    if (submitOutput.innerText !== '') {
-        submitOutput.innerText = ''
+    if (textInfo.innerText !== '') {
+        textInfo.innerText = ''
     }
 
-    let prevBtn = document.getElementById("btn-prev")
-
     saveToStorage()
-    console.log(storage);
+
     s--
     textPertanyaan.innerText = storage[s].pertanyaan
 
-    // JIKA kembali ke pertanyaan 9
     if (s === 8) {
-        // DELETE tombol submit
         let submitBtn = document.getElementById('btn-submit')
         submitBtn.remove()
 
-        // CREATE tombol next
         let nextBtn = document.createElement('button')
         nextBtn.setAttribute('type', 'button')
         nextBtn.setAttribute('class', 'func-btn')
         nextBtn.setAttribute('onclick', 'nextButton()')
         nextBtn.setAttribute('id', 'btn-next')
         nextBtn.innerText = 'next'
-        // PREVIEW created html: <button type="button" onclick="nextButton()" id="btn-next">next</button>
+        // PREVIEW created html: <button type="button" class="func-btn" onclick="nextButton()" id="btn-next">next</button>
 
         buttons.append(nextBtn)
     }
@@ -169,30 +233,33 @@ function prevButton() {
     readCheckedRadio()
 }
 
+/**CONDITION:
+ * MAX index 9
+ * JIKA index 9, MAKA 
+ *      DELETE tombol next
+ *      CREATE tombol submit 
+ */
 function nextButton() {
     if (s >= 9) {
         return
     }
 
     saveToStorage()
-    console.log(storage);
+
     s++
     textPertanyaan.innerText = storage[s].pertanyaan
 
-    // JIKA menuju ke pertanyaan 10
     if (s === 9) {
-        // DELETE tombol next
         let nextBtn = document.getElementById("btn-next")
         nextBtn.remove()
 
-        // CREATE tombol submit
         let submitBtn = document.createElement('button')
         submitBtn.setAttribute('type', 'button')
         submitBtn.setAttribute('class', 'func-btn')
         submitBtn.setAttribute('onclick', 'submitButton()')
         submitBtn.setAttribute('id', 'btn-submit')
         submitBtn.innerText = 'submit'
-        // PREVIEW created html: <button type="button" onclick="submitButton()" id="btn-submit">submit</button>
+        // PREVIEW created html: <button type="button" class="func-btn" onclick="submitButton()" id="btn-submit">submit</button>
 
         buttons.append(submitBtn)
     }
